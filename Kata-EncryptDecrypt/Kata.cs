bcc0ee0d90c52d8c72d35d77190e626592d07673
myasinK <yasinul@gmail.com>
2018-09-20 21:39:39 -0700

    Kata-string manipulation

---
+++ Kata-EncryptDecrypt/Kata.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+
+namespace Kata_EncryptDecrypt
+{
+    public static class Kata
+    {
+        public static string Decrypt(string encryptedText, int n = 0)
+        {
+            if (encryptedText == null)
+            {
+                string.IsNullOrEmpty("sd");
+                throw new Exception("STRING CANNOT BE NULL");
+            }
+            else
+            {
+            if (n >= 1)
+            {
+                Console.WriteLine("String to decrypt: {0}", encryptedText);
+                var sb = new StringBuilder(encryptedText);
+                var halfLength = sb.Length / 2;
+                var halfLengthIndex = halfLength - 1;
+                for (var j = 0; j < n; j++)
+                {
+                for (var i = halfLengthIndex; i >= 0; i--)
+                {
+                    var moveChar = sb[i];
+                    var goesToIndex = (i * 2) + 2;
+                    sb.Insert(goesToIndex, moveChar.ToString(), 1);
+                    sb.Remove(i,1);
+
+                }
+                }
+                Console.WriteLine("Decrypted string is {0}",sb);
+                return sb.ToString();
+            }
+            else 
+            {
+                return encryptedText;
+            }
+            }
+        }
+
+        public static string Encrypt(string text, int n = 0)
+        {
+            if (text == null)
+            {
+                throw new Exception("STRING CANNOT BE NULL");
+            }
+            else
+            {
+            if (n >= 1)
+            {
+                Console.WriteLine("String to encrypt: {0}", text);
+                var sb = new StringBuilder(text);
+                for (var j = 0; j < n; j++)
+                {
+                    var startIndex = 0;
+                    for (var i = 0; i < text.Length; i++)
+                    {
+                        if (i % 2 != 1) continue;
+                        var insertChar = sb[i];
+                        sb.Remove(i, 1);
+                        sb.Insert(startIndex, insertChar.ToString(), 1);
+                        startIndex++;
+                    }
+                }
+
+                Console.WriteLine("Encrypted string is {0}", sb);
+                return sb.ToString();
+            }
+            else
+            {
+                return text;
+            }
+            }
+
+        }
+    }
+}
---
+++ Kata-EncryptDecrypt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//    For building the encrypted string:
+//    Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+//Do this n times!
+
+//Examples:
+
+//"This is a test!", 1 -> "hsi  etTi sats!"
+//"This is a test!", 2 -> "hsi  etTi sats!" -> "s eT ashi tist!"
+
+namespace Kata_EncryptDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "0123456789012345678901234";
+            int n = 4;
+            Kata.Encrypt(null, 1);
+            Kata.Decrypt(null, 1);
+        }
+    }
+}
